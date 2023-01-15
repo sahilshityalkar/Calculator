7892eac6477f7e2914b5cd59350d69c97533b8cf
sahil <sahilshityalkar05@gmail.com>
2023-01-15 22:33:14 +0530

    add the function that can do opretions

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1. Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, 
+// so start by creating functions for the following items and 
+// testing them in your browser’s console.
+// console.log(addition(9, 7))
+// console.log(subtraction(9, 8))
+// console.log(multiplication(7, 6))
+// console.log(division(3, 4))
+
+// Sum opretion
+function addition(a, b) {
+    return a + b;
+};
+
+// Subtraction opretion
+function subtraction(a, b) {
+    return a - b;
+};
+
+// Multiply opretion
+function multiplication(a, b) {
+    return a * b;
+};
+
+// Divide opretion
+function division(a, b) {
+    return a / b;
+};
+
+// percentage opretion
+function percentage(a){
+  return a % a;
+}
